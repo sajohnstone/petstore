bbff9ead93a30ea920b037961e91fbc950dba067
stu <sajohnstone@yahoo.co.uk>
2020-04-27 19:45:27 +0100

    fixed mm project pom issue

--- petstore-web/src/main/java/com/petstore/app/PetstoreWebApplication.java
+++ petstore-web/src/main/java/com/petstore/app/PetstoreWebApplication.java
@@ -2,7 +2,9 @@ package com.petstore.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.petstore")
 @SpringBootApplication
 public class PetstoreWebApplication {
 
